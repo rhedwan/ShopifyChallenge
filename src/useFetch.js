2f8852e29d88300c189dbb61357175ffa6ec164a
rhedwan <adesolaridwan2003@gmail.com>
2022-01-11 23:59:36 +0100

    fixed bug

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,7 +4,7 @@ export const useFetch = (url) => {
   const [data, setData] = useState([]);
 
   const fetchData = useCallback(
-    async (url) => {
+    async () => {
       const response = await fetch(url);
       const data = await response.json();
       setData(data);
